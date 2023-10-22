3c7a441d4a91fdd1f19f6d0b4305945f608cf08c
ali778866 <mdali.ali76@gmail.com>
2023-10-22 10:19:20 +0530

    signup and login html added

---
+++ view/login.js
@@ -0,0 +1,21 @@
+let eyeicon = document.getElementById("eyeicon");
+let password = document.getElementById("password");
+
+eyeicon.onclick = () => {
+    if(password.type === "password") {
+        password.type = "text";
+        eyeicon.src = "/css/eye-open.png"
+    } else {
+        password.type = "password";
+        eyeicon.src="/css/eye-close.png"
+    }
+}
+
+async function logInUser(event) {
+    event.preventDefault()
+    const email = event.target.email.value;
+    const password = event.target.password.value;
+    const user = {
+        email, password
+    }
+}
---
+++ view/signup.js
@@ -0,0 +1,23 @@
+let eyeicon = document.getElementById("eyeicon");
+let password = document.getElementById("password");
+
+eyeicon.onclick = () => {
+    if(password.type === "password") {
+        password.type = "text";
+        eyeicon.src = "/css/eye-open.png"
+    } else {
+        password.type = "password";
+        eyeicon.src="/css/eye-close.png"
+    }
+}
+
+function saveUser(event) {
+    event.preventDefault()
+    const name = event.target.name.value;
+    const email = event.target.email.value;
+    const phone = event.target.phone.value
+    const password = event.target.password.value;
+    const user = {
+        name, email, phone, password
+    }
+}
